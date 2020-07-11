4b25147c0e9e10e11586a04f5754486a93b32fdd
Joseph Trettevik <josephtrettevik@gmail.com>
2020-07-11 12:00:52 -0700

    add button to reset game

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import BouncingBalls from './components/BouncingBalls/BouncingBalls';
 
 function App() {
   const [ move, setMove ] = useState(false);
+  const [ startBtnLabel, setStartBtnLabel ] = useState(true);
 
   const handleClickStart = () => {
     setMove(move => !move);
+    setStartBtnLabel(startBtnLabel => !startBtnLabel);
+  }
+
+  const handleClickReset = () => {
+    setMove(false);
+    setStartBtnLabel(true);
   }
 
   return (
     <div className="App">
       <BouncingBalls className="ballContainer" move={move}/>
-      <Counter className="counter" handleClickStart={handleClickStart}/>
+      <Counter 
+        className="counter" 
+        handleClickStart={handleClickStart} 
+        handleClickReset={handleClickReset} 
+        startBtnLabel={startBtnLabel}
+      />
     </div>
   );
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -3,25 +3,30 @@ import React, { useState } from "react";
 import './Counter.scss';
 
 const Counter= (props) => {
-  // when component renders for first time
-  const initialValue = 0;
-
   // state declaration holding [value, updateFunction]
-  const [stateValue, stateUpdater] = useState(initialValue);
+  const [counter, setCounter] = useState(0);
 
-
+  const handleClickReset = () => {
+    setCounter(0);
+    props.handleClickReset();
+  }
 
   // component render
   return (
     <div className="counterContainer">
       {/* lets display our state in a p tag */}
-      <p>{stateValue}</p>
+      <p>{counter}</p>
 
       {/* lets provide a button to update our state */}
-      <button onClick={() => stateUpdater(previousState => previousState + 1)}>
+      <button onClick={() => setCounter(previousCount => previousCount + 1)}>
         Count
       </button>
-      <button onClick={() => props.handleClickStart()} >Start Game</button>
+      <div className="startGameContainer" >
+        <button onClick={() => props.handleClickStart()} >
+          {props.startBtnLabel ? 'Start Game': 'Stop Game'}
+        </button>
+        <button onClick={() => handleClickReset()}>Reset Game</button>
+      </div>
     </div>
   );
 };
